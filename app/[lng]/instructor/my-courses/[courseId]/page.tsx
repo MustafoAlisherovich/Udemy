ab345cc225bfb94700acd4ac6442739183cfcbfa
MustafoAlisherovich <mustafoalisherovic@gmail.com>
2025-02-03 23:10:18 +0500

    edited my-course

--- app/[lng]/instructor/my-courses/[courseId]/page.tsx
+++ app/[lng]/instructor/my-courses/[courseId]/page.tsx
@@ -1,24 +1,10 @@
-import { getCourseById } from '@/actions/course.action'
-import Header from '../../_components/header'
-import Actions from './_components/actions'
-import { Separator } from '@/components/ui/separator'
-
-async function Page({ params }: { params: { courseId: string } }) {
-	const courseJSON = await getCourseById(params.courseId)
-	const course = JSON.parse(JSON.stringify(courseJSON))
-
+function Page() {
 	return (
-		<>
-			<div className='flex items-center justify-between'>
-				<Header
-					title={course.title}
-					description='Manage Your course and see how it is performing.'
-				/>
-				<Actions {...course} />
-			</div>
-			<Separator className='my-3 bg-muted-foreground' />
-		</>
+		<div>
+			
+		</div>
 	)
 }
 
 export default Page
+
